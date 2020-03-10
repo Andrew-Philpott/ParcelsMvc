69ce2f9257c0f8fe155c620bdbf5a04e8e4f0e40
Andrew Philpott <andrewphilpott92@gmail.com>
2020-03-10 11:14:39 -0700

    Add parcel methods.

--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -1,9 +1,51 @@
 using System;
 using System.Collections.Generic;
 
-namespace Parcels.Models {
+namespace Parcels.Models
+{
+
   public class Parcel
   {
-   
+    public int Width { get; }
+
+    public int Length { get; }
+
+    public int Depth { get; }
+    public double Weight { get; }
+    public Parcel(int width, int length, int depth, double weight)
+    {
+      Width = width;
+      Length = length;
+      Depth = depth;
+      Weight = weight;
+    }
+
+    public int Volume()
+    {
+      return (Width * Length * Depth);
+    }
+
+    public double CostToShip()
+    {
+      int volume = Volume();
+      int sizeMultiplier = 0;
+      if (volume < 20)
+      {
+        sizeMultiplier = 2;
+      }
+      else if (volume >= 20 && volume <= 50)
+      {
+        sizeMultiplier = 3;
+      }
+      else if (volume <= 100)
+      {
+        sizeMultiplier = 4;
+      }
+      else
+      {
+        sizeMultiplier = 7;
+      }
+      return sizeMultiplier * volume;
+    }
   }
 }
